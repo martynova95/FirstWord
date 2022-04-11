5c0884d5050389ca7ac1be09f170dbc3dc00b393
martynova <antipovamasha2009@bk.ru>
2022-04-11 22:25:26 +0300

    Домашняя работа 1

--- src/com/homeproject/firstproject/HomeWorkApp.java
+++ src/com/homeproject/firstproject/HomeWorkApp.java
@@ -49,7 +49,7 @@ public class HomeWorkApp {
 
     public static void compareNumbers() {
         int a = 6;
-        int b = 19;
+        int b = 18;
 
         if (a >= b) {
             System.out.println("a>=b");
